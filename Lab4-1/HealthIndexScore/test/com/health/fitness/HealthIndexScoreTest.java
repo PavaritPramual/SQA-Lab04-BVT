1ab2d1f395d32ff8fe521b3d36d0817a51952fb7
PavaritPramual <pavarit.p@kkumail.com>
2026-07-18 10:35:38 +0700

    feat: complete Lab 4 BVT, implement JUnit tests, and document test results
    - Add designed test cases (Lab4_BVT.xlsx)
    - Implement BVT test suite using JUnit 5
    - Record actual test results and defect reports in templates

---
+++ Lab4-1/HealthIndexScore/test/com/health/fitness/HealthIndexScoreTest.java
@@ -0,0 +1,36 @@
+package com.health.fitness;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.health.fitness.HealthIndexScore.FitnessLevel;
+
+// Normal test
+class HealthIndexScoreTest {
+	
+	
+	@ParameterizedTest(name = "{0} - Inputs: VO2={1}, RHR={2}, HRR={3} => Expected: {4}")
+	@CsvSource({
+	    "TC001, 52.0, 40, 15, EXCELLENT",
+	    "TC002, 52.0, 41, 15, STANDARD",
+	    "TC003, 52.0, 130, 15, STANDARD",
+	    "TC004, 52.0, 119, 15, STANDARD",
+	    "TC005, 52.0, 220, 15, STANDARD",
+	    "TC006, 0.0, 130, 15, POOR",
+	    "TC007, 1.0, 130, 15, POOR",
+	    "TC008, 102.0, 130, 15, STANDARD",
+	    "TC009, 103.0, 130, 15, STANDARD",
+	    "TC010, 52.0, 130, 0, STANDARD",
+	    "TC011, 52.0, 130, 1, STANDARD",
+	    "TC012, 52.0, 130, 29, STANDARD",
+	    "TC013, 52.0, 130, 30, STANDARD"
+	})
+	void testFinestLevel(String id, double vo2, int rhr, int hrr, FitnessLevel expected) {
+	    
+	    HealthIndexScore scoreCalculator = new HealthIndexScore(vo2, rhr, hrr);
+	     
+	    assertEquals(expected, scoreCalculator.getFitnessLevel(), "Failed at " + id);
+	}
+}
