2b0372d61a8a5c959d5883e04ba2720eaf9fff17
PavaritPramual <pavarit.p@kkumail.com>
2026-07-18 12:24:48 +0700

    feat: complete Lab 4.2 Robustness Testing

--- Lab4-2/HealthIndexScore/src/test/java/com/health/fitness/HealthIndexScoreRobustnessTest.java
+++ Lab4-2/HealthIndexScore/src/test/java/com/health/fitness/HealthIndexScoreRobustnessTest.java
@@ -1,17 +1,14 @@
 package com.health.fitness;
-
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import com.health.fitness.HealthIndexScore.FitnessLevel;
+class HealthIndexScoreRobustnessTest {
 
-// Normal boundary value testing
-class HealthIndexScoreTest {
-	
-	
-	@ParameterizedTest(name = "{0} - Inputs: VO2={1}, RHR={2}, HRR={3} => Expected: {4}")
+	// withVALID
+	@ParameterizedTest(name = "Normal: {0} - Inputs: VO2={1}, RHR={2}, HRR={3} => Expected: {4}")
 	@CsvSource({
 	    "TC001, 52.0, 40, 15, EXCELLENT",
 	    "TC002, 52.0, 41, 15, STANDARD",
@@ -25,12 +22,28 @@ class HealthIndexScoreTest {
 	    "TC010, 52.0, 130, 0, STANDARD",
 	    "TC011, 52.0, 130, 1, STANDARD",
 	    "TC012, 52.0, 130, 29, STANDARD",
-	    "TC013, 52.0, 130, 30, STANDARD"
+	    "TC013, 52.0, 130, 30, STANDARD",
+	    "TC015, 104.0, 130, 15, STANDARD",
+	    "TC019, 52.0, 130, 31, STANDARD"
+	    
 	})
-	void testFinestLevel(String id, double vo2, int rhr, int hrr, FitnessLevel expected) {
+	void testFinestLevelwithVALID(String id, double vo2, int rhr, int hrr, FitnessLevel expected) {
 	    
 	    HealthIndexScore scoreCalculator = new HealthIndexScore(vo2, rhr, hrr);
 	     
 	    assertEquals(expected, scoreCalculator.getFitnessLevel(), "Failed at " + id);
+	}
+	
+	// withINVALID
+	@ParameterizedTest(name = "Robust: {0} - VO2={1}, RHR={2}, HRR={3} => Expected: IllegalArgumentException")
+    @CsvSource({
+        "TC014, -1.0, 130, 15",
+        "TC016, 52.0, 39, 15",
+        "TC017, 52.0, 221, 15",
+        "TC018, 52.0, 130, -1"  
+    })
+	void testFinestLevelwithINVALID(String id, double vo2, int rhr, int hrr) {
+		assertThrows(IllegalArgumentException.class, 
+				() -> new HealthIndexScore(vo2, rhr, hrr));
 	}
 }
